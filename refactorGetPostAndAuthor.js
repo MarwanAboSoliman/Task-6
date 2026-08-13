54ded8a10678986b030d55721a32d69a227d61ef
MarwanAboSoliman <marawanalaam@gmail.com>
2026-08-13 14:04:46 +0300

    task6

--- refactorGetPostAndAuthor.js
+++ refactorGetPostAndAuthor.js
@@ -13,20 +13,19 @@
 
 // console.log(getPostAndAuthor(1));
 
-
-const getPostAndAuthor = async (postId) =>{
-  try{
-    
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+const getPostAndAuthor = async (postId) => {
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${postId}`,
+    );
     const post = await response.json();
-      const userRes = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
-      const user = await userRes.json()
-      console.log(`"${post.title}" by "${user.name}"`);
-      
-  }catch(err){
+    const userRes = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${post.userId}`,
+    );
+    const user = await userRes.json();
+    console.log(`"${post.title}" by "${user.name}"`);
+  } catch (err) {
     console.log(err);
-    
   }
-}
+};
 console.log(getPostAndAuthor(1));
-
